1e66f0b5c8556ff6db6fb6e780935aca3016a77d
tpdms0159 <tpdms0159@naver.com>
2023-10-23 22:08:06 +0900

    final 2

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://ec2-52-78-9-158.ap-northeast-2.compute.amazonaws.com/",
+      target: "https://ec2-52-78-9-158.ap-northeast-2.compute.amazonaws.com",
       changeOrigin: true,
     })
   );
